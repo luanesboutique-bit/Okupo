5b7f703a29af17ac7326f7d49a7e89ba42ae2ddf
luanesboutique-bit <luanesboutique@gmail.com>
2026-04-29 17:31:29 -0600

    Update configuracion.py

--- src/infraestructura/configuracion.py
+++ src/infraestructura/configuracion.py
@@ -1,4 +1,4 @@
 import os
 
 CLAVE_SECRETA = os.environ.get('CLAVE_SECRETA', 'clave_secreta_okupo_2024')
-URL_BASE_API = os.environ.get('URL_BASE_API', "https://0d078abf91f17ed8-189-203-204-127.serveousercontent.com")
+URL_BASE_API = os.environ.get('URL_BASE_API' "https://b262606dae6d6c90-189-203-204-127.serveousercontent.com")
